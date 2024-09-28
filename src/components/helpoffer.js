63bd90bf2d726dbf571e29d3c2471979d81b78de
santhosh <santhoshprince6@gmail.com>
2024-09-28 16:56:38 +0530

    help offfer changes

--- src/components/helpoffer.js
+++ src/components/helpoffer.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import logo from "../img/logo/capstonelogo.png"
-import backgroundImage from "../img/bg/widget_bg_1.jpg"
+import logo from '../img/logo/capstonelogo.png';
+import backgroundImage from '../img/bg/widget_bg_1.jpg';
+import './HelpOffer.css'; // Import the CSS file
+
 const HelpOffer = () => {
   return (
-    <div className="widget widget_offer wide-new" style={{ backgroundImage: `url(${backgroundImage})` }}  >
+    <div className="widget widget_offer wide-new" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="offer-banner">
-        <div className="offer">
+        <div className="offer-content">
           <h6 className="box-title">Need Help? We Are Here To Help You</h6>
           <div className="banner-logo">
             <img src={logo} alt="Tourm" />
           </div>
-          <div className="offer">
-            <h6 className="offer-title">You Get Online support</h6>
-            <a className="offter-num" href="tel:+256214203215">+256 214 203 215</a>
+          <div className="offer-details">
+            <h6 className="offer-title">You Get Online Support</h6>
+            <a className="offer-num" href="tel:+256214203215">+256 214 203 215</a>
           </div>
           <a href="contact.html" className="th-btn style2 th-icon">Read More</a>
         </div>
