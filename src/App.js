07d2bae3a6bcbe4e3b86ae289a970476edac4d75
santhosh <santhoshprince6@gmail.com>
2024-10-14 16:16:11 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from "./pages/aboutus";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import BlogDetails from "./pages/blogdeatils";
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 import Bali from "./desinationpage/bali";
