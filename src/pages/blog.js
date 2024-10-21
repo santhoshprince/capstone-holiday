78f45bf8a2da9ca1771bb3786758f35194a8c3e3
santhosh <santhoshprince6@gmail.com>
2024-10-21 14:32:09 +0530

    correction fixed

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,11 +4,18 @@ import Pagination from "../components/blogpagination";
 import ContentSection from "../components/contentbg";
 import Sidebar from "../components/sidebar";
 import blogbg from "../assets/blogsimg/city-4864747_1920.jpg";
+import { Helmet } from 'react-helmet';
 
 
 const BlogPage = () => {
   return (
     <>
+     <Helmet>
+        <title>Blogs</title>
+        {/* <meta name="keywords" content="andaman tour, andaman tourism itinerary, andaman island, andaman tour plan" /> */}
+        {/* <meta name="description" content="The Andaman tour from Chennai will take you to Andaman after monsoon days. So, Capstone Holidays plans for an Andaman trip from October to April." /> */}
+       
+      </Helmet>
      <ContentSection heading={"Blogs"} children={""} backgroundImage={blogbg} />
       <section className="th-blog-wrapper space-top space-extra-bottom">
         <div className="container">
